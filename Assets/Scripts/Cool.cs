cbe151fb7a1bbc10592f696a1b112ea1f76717c7
Lovisa Svensson <77107017+spookypannkaka@users.noreply.github.com>
2023-04-05 16:19:14 +0200

    Början på två monitors, nu kan man använda två displays

---
+++ Assets/Scripts/Cool.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cool : MonoBehaviour
+{
+    [SerializeField] float rotationAmount = 0.1f;
+    float random = 0.2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0,0,rotationAmount);
+    }
+}
