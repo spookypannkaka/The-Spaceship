c0e641f73d4fa696f358e36bf11af4f9f4d789f2
Luckelina <93657078+Luckelina@users.noreply.github.com>
2023-05-29 20:54:34 +0200

    final changes
    massa olika saker

--- Assets/Scripts/ActivateScreens.cs
+++ Assets/Scripts/ActivateScreens.cs
@@ -5,14 +5,13 @@ public class ActivateScreens : MonoBehaviour
 {
     void Start ()
     {
-        Debug.Log ("displays connected: " + Display.displays.Length);
-            // Display.displays[0] is the primary, default display and is always ON, so start at index 1.
-            // Check if additional displays are available and activate each.
-    
+        //Activate all connected screens
         for (int i = 1; i < Display.displays.Length; i++)
             {
                 Display.displays[i].Activate();
             }
+        //Hide the mouse :)
+        Cursor.visible = false;
     }
     
     void Update()
